9d4754831d8b2497b186f98ed1b65ca887cc49aa
mehedi <mehedi.hasan@morzaa.com>
2025-10-27 09:13:00 +0600

    initial setup

---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Categories } from "../../ecommerce/pages/categories/categories";
+
+@Component({
+  selector: 'app-header',
+  imports: [Categories],
+  templateUrl: './header.html',
+})
+export class Header {
+
+}
