2a485d0a552f1171bc74fe1d5718bf9d961a59d2
mehedi <mehedi.hasan@morzaa.com>
2025-10-27 18:11:35 +0600

    add master service

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl : '/api'
+  };
+  // for development run by it
+  // npx ng serve --proxy-config proxy.conf.json 
---
+++ src/app/shared/services/master.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MasterService {
+  
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  // GET request method
+  get<T>(endpoint: string): Observable<T> {
+    const url = `${this.apiUrl}/${endpoint}`;
+    return this.http.get<T>(url);
+  }
+
+  // POST request method
+  post<T>(endpoint: string, data: any): Observable<T> {
+    const url = `${this.apiUrl}/${endpoint}`;
+    return this.http.post<T>(url, data);
+  }
+
+  // PUT request method
+  put<T>(endpoint: string, data: any): Observable<T> {
+    const url = `${this.apiUrl}/${endpoint}`;
+    return this.http.put<T>(url, data);
+  }
+
+  // DELETE request method
+  delete<T>(endpoint: string): Observable<T> {
+    const url = `${this.apiUrl}/${endpoint}`;
+    return this.http.delete<T>(url);
+  }
+}
