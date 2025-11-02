f0ecc8652fd15eb0ad35466863c146c0655dc421
shahriyaar <shahriar.fahim@morzaa.com>
2025-11-02 14:34:58 +0600

    color value added

--- src/app/ecommerce/pages/categories/categories.ts
+++ src/app/ecommerce/pages/categories/categories.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-categories',
   imports: [],
   templateUrl: './categories.html',
+  styleUrl:'./categories.css'
 })
 export class Categories {
 
