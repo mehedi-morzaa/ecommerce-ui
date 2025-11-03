9d94fb5b968750724acc622e921c2cd1389fa6ee
shahriyaar <shahriar.fahim@morzaa.com>
2025-11-03 17:59:06 +0600

    terms, privacy, purchase dynamically converted

--- src/app/ecommerce/pages/terms/terms.page.ts
+++ src/app/ecommerce/pages/terms/terms.page.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { HtmlService } from '../../../services/html.service';
+import { HtmlContent } from '../../../models/html-content.model';
 
 @Component({
   selector: 'page-terms',
   imports: [],
   templateUrl: './terms.page.html',
 })
-export class TermsPage {
+export class TermsPage implements OnInit {
 
+  /* ========================================================
+   🔧 CONSTRUCTOR & DEPENDENCY INJECTION
+======================================================== */
+  constructor(private htmlService: HtmlService) {}
+
+  /* ========================================================
+   🧩 PROPERTIES (State / Data)
+======================================================== */
+  termsContent = signal<HtmlContent[] | null>(null);
+
+
+  /* ========================================================
+   🛠️ METHODS
+======================================================== */
+  LoadTerms() {
+    this.htmlService.getListofContent(3, 4, 1).subscribe({
+      next: (res) => {
+        if (res.success) {
+          this.termsContent.set(res.data);
+          console.log(res.data);
+        }
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+
+
+  /* ========================================================
+   🌀 LIFECYCLE HOOKS
+======================================================== */
+  ngOnInit(): void {
+    this.LoadTerms();
+  }
 }
